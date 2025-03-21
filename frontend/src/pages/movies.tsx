a2680a1eeb81d386da587489e4419091d7e577ca
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-03-21 22:00:47 +0100

    Fleshed out pages for each media type

---
+++ frontend/src/pages/movies.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import Navbar from "../components/navbar";
+
+const Movies = () => {
+  return (
+    <div className="home">
+        <Navbar/>
+    </div>
+  );
+};
+
+export default Movies;
---
+++ frontend/src/pages/series.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import Navbar from "../components/navbar";
+
+const Series = () => {
+  return (
+    <div className="home">
+        <Navbar/>
+    </div>
+  );
+};
+
+export default Series;
