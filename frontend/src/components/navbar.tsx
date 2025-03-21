a9b846bce84868c5bc1b12b05d0270c12be19a17
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-03-21 22:04:46 +0100

    Created general navbar component and corrisponfing css

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,38 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <header>
+          <div className="navbar-container">
+            <NavLink 
+            className={({ isActive }) => isActive ? "active-link" : "nav-link"} 
+            to="/">
+              <h1 className="navbar-title">Home</h1>
+            </NavLink>
+            <div className="media-pages-container">
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link books active-link-book" : "media-page-link books"} to="/books"> 
+                Books
+              </NavLink>
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link manga active-link-manga" : "media-page-link manga"} to="/manga">
+                Manga
+              </NavLink>
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link series active-link-series" : "media-page-link series"} to="/series">
+                Series
+              </NavLink>
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link anime active-link-anime" : "media-page-link anime"} to="/anime">
+                Anime
+              </NavLink>
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link games active-link-games" : "media-page-link games"} to="/games">
+                Games
+              </NavLink>
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link movies active-link-movies" : "media-page-link movies"} to="/movies">
+                Movies
+              </NavLink>
+            </div>
+            <button className="login-button"><p>+</p></button>
+          </div>
+        </header>
+    );
+}
+
+export default Navbar;
