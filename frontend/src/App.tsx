3dc2a841ed9fb379a0d163671c8dbc3ae5d4dea2
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-03-21 22:05:50 +0100

    Fleshed out the app routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// # =========== PAGES & COMPONENTS =========== #
+import Home from './pages/home';
+import Books from './pages/books';
+import Manga from './pages/manga';
+import Movies from './pages/movies';
+import Series from './pages/series';
+import Anime from './pages/anime';
+import Games from './pages/games';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/books' element={<Books/>} />
+          <Route path='/manga' element={<Manga/>} />
+          <Route path='/movies' element={<Movies/>} />
+          <Route path='/series' element={<Series/>} />
+          <Route path='/anime' element={<Anime/>} />
+          <Route path='/games' element={<Games/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
