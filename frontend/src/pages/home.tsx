55fd8a1879b35d72fabcefb04a5376845174eb15
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-03-22 16:36:28 +0100

    added test fields to home

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,15 +1,32 @@
 import { useEffect, useState } from "react";
+import "../styles/home.css";
 import Navbar from "../components/navbar";
 import ListElement from "../components/listElement";
+import HomeMediaSect from "../components/homeMediaSection";
+
 
 const Home = () => {
   return (
     <div className="home">
         <Navbar />
-        <ListElement defaultClass="books" totUnits={100} completedUnits={10} status="Planned" genre={"Genre 1"} saga={"Saga 1"}/>
-        <ListElement defaultClass="books" totUnits={100} completedUnits={10} status="Planned" genre={"Genre 1"} saga={"Saga 1"}/>
-        <ListElement defaultClass="books" totUnits={100} completedUnits={10} status="Planned" genre={"Genre 1"} saga={"Saga 1"}/>
-        <ListElement defaultClass="books" totUnits={100} completedUnits={10} status="Planned" genre={"Genre 1"} saga={"Saga 1"}/>
+        <HomeMediaSect defaultCss="books" itemsFound={100}>
+          <ListElement defaultClass="books" totUnits={1000} completedUnits={100} status="Planned" genre={"Genre 1"} saga={"Saga 1"}/>
+        </HomeMediaSect>
+        <HomeMediaSect defaultCss="manga" itemsFound={100}>
+          <ListElement defaultClass="manga" totUnits={1000} completedUnits={100} status="Planned" genre={"Genre 1"} saga={"Saga 1"}/>
+        </HomeMediaSect>
+        <HomeMediaSect defaultCss="series" itemsFound={100}>
+          <ListElement defaultClass="series" totUnits={1000} completedUnits={100} status="Planned" genre={"Genre 1"} saga={"Saga 1"}/>
+        </HomeMediaSect>
+        <HomeMediaSect defaultCss="anime" itemsFound={100}>
+          <ListElement defaultClass="anime" totUnits={1000} completedUnits={100} status="Planned" genre={"Genre 1"} saga={"Saga 1"}/>
+        </HomeMediaSect>
+        <HomeMediaSect defaultCss="games" itemsFound={100}>
+          <ListElement defaultClass="games" totUnits={1000} completedUnits={100} status="Planned" genre={"Genre 1"} saga={"Saga 1"}/>
+        </HomeMediaSect>
+        <HomeMediaSect defaultCss="movies" itemsFound={100}>
+          <ListElement defaultClass="movies" totUnits={1000} completedUnits={100} status="Planned" genre={"Genre 1"} saga={"Saga 1"}/>
+        </HomeMediaSect>
     </div>
   );
 };
