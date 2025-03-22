e11bf137f6bdf68b967724f945b6bc6644c935d9
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-03-22 02:43:02 +0100

    Fleshed out the list-element component ; added SVGs to assets/ ; added support for SVGs

---
+++ frontend/src/components/listElement.tsx
@@ -0,0 +1,86 @@
+import { useLocation } from "react-router-dom";
+import "../styles/listElement.css";
+import { ReactComponent as SvgCounterDot } from "../assets/counterDot.svg";
+import { ReactComponent as SvgAddPage } from "../assets/addPage.svg";
+import CircularProgressBar from "./circularProgressBar";
+
+const ListElement = (
+  {
+    defaultClass,
+    totUnits,
+    completedUnits,
+    status,
+    genre,
+    saga
+  }:
+  {
+    defaultClass: string,
+    totUnits: number,
+    completedUnits: number,
+    status : string,
+    genre: string,
+    saga: string
+  }
+) => {
+    const location = useLocation(); // Get current URL path
+
+    // Define an explicit mapping of paths to colors
+    const pathColors: Record<string, string> = {
+        "/games": "games",
+        "/anime": "anime",
+        "/movies": "movies",
+        "/books": "books",
+        "/music": "music",
+        "/sports": "sports",
+    };
+    // Use a fallback color if the pathname isn't in the mapping
+    const cssClass = pathColors[location.pathname] || defaultClass;
+    
+    const statusColors: Record<string, string> = {
+      "Active": cssClass,
+      "On Hold": "on-hold",
+      "Dropped": "dropped",
+      "Planned": "planned",
+      "Completed": "completed",
+    };
+
+    const statusClass = statusColors[status] || "";
+
+    return (
+        <div className={"list-element " + cssClass}>
+          <div className="counter">
+            <p className="counter-num" >1</p>
+            <SvgCounterDot className={cssClass}/>
+          </div>
+          <div className="title-space">
+            <h1 className="title">Title one</h1>
+            <p className="author">Author one</p>
+          </div>
+          <div className="progress-counter">
+            <button onClick={() => {}}>
+             <SvgAddPage className={cssClass + " add-page"}/>
+            </button>
+             <div className="progress-num">
+                <span>{completedUnits}</span>
+                <span>/</span>
+                <span>{totUnits}</span>
+             </div>
+             <CircularProgressBar progress={40} cssClass={cssClass}/>
+          </div>
+          <div className="element-field-container">
+            <span className="field-tag">Status</span>
+            <span className={"status "+ statusClass}>{status}</span>
+          </div>
+          <div className="element-field-container">
+            <span className="field-tag">Genre</span>
+            <span className="field">{genre}</span>
+          </div>
+          <div className="element-field-container">
+            <span className="field-tag">Genre</span>
+            <span className="field">{saga}</span>
+          </div>
+        </div>
+    );
+};
+
+export default ListElement;
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
+import ListElement from "../components/listElement";
 
 const Home = () => {
   return (
     <div className="home">
         <Navbar />
+        <ListElement defaultClass="books" totUnits={100} completedUnits={10} status="Planned" genre={"Genre 1"} saga={"Saga 1"}/>
     </div>
   );
 };
