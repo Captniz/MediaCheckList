a57b4f3e937757eb16c8e1e00faffadf16aeaea3
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-03-22 14:24:48 +0100

    fixed navbar error

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -11,22 +11,22 @@ const Navbar = () => {
               <h1 className="navbar-title">Home</h1>
             </NavLink>
             <div className="media-pages-container">
-              <NavLink className={({ isActive }) =>  isActive ? "media-page-link books active-link-book" : "media-page-link books"} to="/books"> 
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link active-link-book" : "media-page-link books"} to="/books"> 
                 Books
               </NavLink>
-              <NavLink className={({ isActive }) =>  isActive ? "media-page-link manga active-link-manga" : "media-page-link manga"} to="/manga">
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link active-link-manga" : "media-page-link manga"} to="/manga">
                 Manga
               </NavLink>
-              <NavLink className={({ isActive }) =>  isActive ? "media-page-link series active-link-series" : "media-page-link series"} to="/series">
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link active-link-series" : "media-page-link series"} to="/series">
                 Series
               </NavLink>
-              <NavLink className={({ isActive }) =>  isActive ? "media-page-link anime active-link-anime" : "media-page-link anime"} to="/anime">
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link active-link-anime" : "anime media-page-link"} to="/anime">
                 Anime
               </NavLink>
-              <NavLink className={({ isActive }) =>  isActive ? "media-page-link games active-link-games" : "media-page-link games"} to="/games">
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link active-link-games" : "media-page-link games"} to="/games">
                 Games
               </NavLink>
-              <NavLink className={({ isActive }) =>  isActive ? "media-page-link movies active-link-movies" : "media-page-link movies"} to="/movies">
+              <NavLink className={({ isActive }) =>  isActive ? "media-page-link active-link-movies" : "media-page-link movies"} to="/movies">
                 Movies
               </NavLink>
             </div>
