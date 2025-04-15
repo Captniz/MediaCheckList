d5bb44ae04090668704e1d408e026347004344fe
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-04-15 12:02:28 +0200

    Completely re-styled the list element, fixed some dimesnion related issues, finished the element style

--- frontend/src/components/listElement.tsx
+++ frontend/src/components/listElement.tsx
@@ -1,92 +1,137 @@
-import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import "../styles/listElement.css";
 import { ReactComponent as SvgCounterDot } from "../assets/counterDot.svg";
 import { ReactComponent as SvgAddPage } from "../assets/addPage.svg";
+import { ReactComponent as SvgSeparator } from "../assets/separator.svg";
 import CircularProgressBar from "./circularProgressBar";
+import ReactMarkdown from 'react-markdown';
 
-const ListElement = (
-  {
-    defaultClass,
-    totUnits,
-    completedUnits,
-    status,
-    genre,
-    saga
-  }:
-  {
-    defaultClass: string,
-    totUnits: number,
-    completedUnits: number,
-    status : string,
-    genre: string,
-    saga: string
-  }
-) => {
-    const location = useLocation(); // Get current URL path
+const ListElement = ({
+	totUnits,
+	completedUnits,
+	status,
+	genre,
+	saga,
+	notes
+}: {
+	totUnits: number;
+	completedUnits: number;
+	status: string;
+	genre: string;
+	saga: string;
+	notes: string;
+}) => {
+	const [isOpen, setIsOpen] = useState(false);
 
-    // Define an explicit mapping of paths to colors
-    const pathColors: Record<string, string> = {
-        "/games": "games",
-        "/anime": "anime",
-        "/movies": "movies",
-        "/books": "books",
-        "/music": "music",
-        "/sports": "sports",
-    };
-    // Use a fallback color if the pathname isn't in the mapping
-    const cssClass = pathColors[location.pathname] || defaultClass;
-    
-    const statusColors: Record<string, string> = {
-      "Active": cssClass,
-      "On Hold": "on-hold",
-      "Dropped": "dropped",
-      "Planned": "planned",
-      "Completed": "completed",
-    };
+	const statusColors: Record<string, string> = {
+		Active: "active",
+		"On Hold": "on-hold",
+		Dropped: "dropped",
+		Planned: "planned",
+		Completed: "completed",
+	};
 
-    const statusClass = statusColors[status] || "";
+	const statusClass = statusColors[status] || "";
 
-    return (
-      <div className={"list-element " + cssClass}>
-        <div className="left-side-container">
-          <div className="counter-containter">
-            <p className="counter-num" >1</p>
-            <SvgCounterDot className={cssClass + " counter-dot"}/>
-          </div>
-          <div className="title-space">
-            <h1 className="title">Book 1</h1>
-            <p className="author">Pippo</p>
-          </div>
-        </div>
-        <div className="center-container">
-          <div className="progress-counter">
-            <button onClick={() => {}}>
-              <SvgAddPage className={cssClass + " add-page"}/>
-            </button>
-            <div className="progress-num">
-              <span>{completedUnits}</span>
-              <span>/</span>
-              <span>{totUnits}</span>
-            </div>
-            <CircularProgressBar progress={40} cssClass={cssClass}/>
-          </div>
-          <div className="status-field-container">
-            <span className="field-tag">Status</span>
-            <span className={"status "+ statusClass}>{status}</span>
-          </div>
-        </div>
-          <div className="right-side-container">
-          <div className="element-field-container">
-            <span className="field-tag">Genre</span>
-            <span className="field">{genre}</span>
-          </div>
-        <div className="element-field-container">
-          <span className="field-tag">Saga</span>
-          <span className="field">{saga}</span>
-        </div>
-      </div>
-    </div>
-    );
+	return (
+		<div
+			className="element-container"
+			style={{ height: isOpen ? "40vh" : "5vh" }}
+		>
+			<div className="list-element" style={{ height: isOpen ? "5vh" : "100%" }}>
+				<div className="left-side-container">
+					<div className="counter-containter">
+						<p className="counter-num">1</p>
+						<button
+							className="collapse-button"
+							style={{ transform: isOpen ? "rotate(90deg)" : "rotate(0deg)" }}
+							onClick={() => setIsOpen(!isOpen)}
+						>
+							<SvgCounterDot className="counter-dot" />
+						</button>
+					</div>
+					<div className="title-space">
+						<h1 className="title">Horus Rising</h1>
+						<p className="author">Inio Asano</p>
+					</div>
+				</div>
+				<div className="center-container">
+					<div className="progress-counter">
+						<button onClick={() => {}}>
+							<SvgAddPage className=" add-page" />
+						</button>
+						<div className="progress-num">
+							<span>{completedUnits}</span>
+							<span>/</span>
+							<span>{totUnits}</span>
+						</div>
+						<CircularProgressBar
+							progress={(completedUnits / totUnits) * 100}
+							cssClass="circ-progress"
+						/>
+					</div>
+					<div className="status-field-container">
+						<span className="field-tag">Status</span>
+						<span className={"status " + statusClass}>{status}</span>
+					</div>
+				</div>
+				<div className="right-side-container">
+					<div className="element-field-container">
+						<span className="field-tag">Genre</span>
+						<span className="field">{genre}</span>
+					</div>
+					<div className="element-field-container">
+						<span className="field-tag">Saga</span>
+						<span className="field">{saga}</span>
+					</div>
+				</div>
+			</div>
+			<div
+				style={{
+					height: isOpen ? "31vh" : "0%",
+					paddingBottom: isOpen ? "1vw" : 0,
+					paddingTop: isOpen ? "1vw" : 0,
+					overflow: "hidden",
+				}}
+				className="dropdown-container"
+			>
+				<div className="container-left-info">
+					<img className="dropdown-image" src="/test.jpg" alt="bho" />
+					<div className="container-central-info">
+						<div className="central-info">
+							<span className="big-title">Horus Rising</span>
+							<div className="subtitle-container">
+								<span className="big-author">Author Name</span>
+								<SvgSeparator className=" separator" />
+								<span className="big-date">1/1/1970</span>
+							</div>
+							<div className="buttons-container">
+								<button className=" action-button">Modify</button>
+								<button className=" action-button">Open</button>
+							</div>
+						</div>
+						<div className="description-area">
+							Under the benevolent leadership of the Immortal Emperor the
+							Imperium of Man has stretched out across the galaxy. On the eve of
+							victory, the Emperor leaves the front lines, entrusting the great
+							crusade to his favorite son, Horus. Promoted to Warmaster, the
+							idealistic Horus tries to carry out the Emperor'sgrand design, all
+							the while the seeds of heresy and rebellion have been sowed
+							amongst his brothers.
+						</div>
+					</div>
+				</div>
+				<div className="container-right-info">
+					<p className="notes-tag">Notes</p>
+					<div className="notes-container">
+						<ReactMarkdown>
+							{notes}
+						</ReactMarkdown>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ListElement;
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import ListElement from "../components/listElement";
 import HomeMediaSect from "../components/homeMediaSection";
@@ -7,64 +6,78 @@ const Home = () => {
 	return (
 		<div className="home">
 			<Navbar />
-			<HomeMediaSect defaultCss="books" itemsFound={100}>
-				<ListElement
-					defaultClass="books"
-					totUnits={1000}
-					completedUnits={100}
-					status="Planned"
-					genre={"Genre 1"}
-					saga={"Saga 1"}
-				/>
-			</HomeMediaSect>
+			<ListElement
+				totUnits={100}
+				completedUnits={50}
+				status="On Hold"
+				genre={"Genre 1"}
+				saga={"Saga 1"}
+				notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
+			/>
+			<ListElement
+				totUnits={100}
+				completedUnits={50}
+				status="On Hold"
+				genre={"Genre 1"}
+				saga={"Saga 1"}
+				notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
+			/>
+			<ListElement
+				totUnits={100}
+				completedUnits={50}
+				status="On Hold"
+				genre={"Genre 1"}
+				saga={"Saga 1"}
+				notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
+			/>
 			<HomeMediaSect defaultCss="manga" itemsFound={100}>
 				<ListElement
-					defaultClass="manga"
 					totUnits={1000}
 					completedUnits={100}
 					status="Planned"
 					genre={"Genre 1"}
 					saga={"Saga 1"}
+					notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
 				/>
 			</HomeMediaSect>
 			<HomeMediaSect defaultCss="series" itemsFound={100}>
 				<ListElement
-					defaultClass="series"
 					totUnits={1000}
 					completedUnits={100}
-					status="Planned"
+					status="Active"
 					genre={"Genre 1"}
 					saga={"Saga 1"}
+					notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
 				/>
 			</HomeMediaSect>
 			<HomeMediaSect defaultCss="anime" itemsFound={100}>
 				<ListElement
-					defaultClass="anime"
 					totUnits={1000}
 					completedUnits={100}
-					status="Planned"
+					status="Completed"
 					genre={"Genre 1"}
 					saga={"Saga 1"}
+					notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
 				/>
 			</HomeMediaSect>
 			<HomeMediaSect defaultCss="games" itemsFound={100}>
 				<ListElement
-					defaultClass="games"
 					totUnits={1000}
 					completedUnits={100}
 					status="Planned"
 					genre={"Genre 1"}
 					saga={"Saga 1"}
+					notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
 				/>
 			</HomeMediaSect>
 			<HomeMediaSect defaultCss="movies" itemsFound={100}>
 				<ListElement
-					defaultClass="movies"
 					totUnits={1000}
 					completedUnits={100}
 					status="Planned"
 					genre={"Genre 1"}
 					saga={"Saga 1"}
+					notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
 				/>
 			</HomeMediaSect>
 		</div>
