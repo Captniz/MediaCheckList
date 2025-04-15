3e4acc4f1febc02cb5625d5175488d7f2c8d29fc
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-04-15 12:01:05 +0200

    simplified navbar style/layout

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -6,73 +6,61 @@ const Navbar = () => {
 		<header>
 			<div className="navbar-container">
 				<NavLink
-					className={({ isActive }) => (isActive ? "active-link" : "nav-link")}
+					className={({ isActive }) =>
+						isActive ? "active-media-page-link" : ""
+					}
 					to="/"
 				>
 					<h1 className="navbar-title">Home</h1>
 				</NavLink>
-				<div className="media-pages-container">
-					<NavLink
-						className={({ isActive }) =>
-							isActive
-								? "media-page-link active-link-book"
-								: "media-page-link books"
-						}
-						to="/books"
-					>
-						Books
-					</NavLink>
-					<NavLink
-						className={({ isActive }) =>
-							isActive
-								? "media-page-link active-link-manga"
-								: "media-page-link manga"
-						}
-						to="/manga"
-					>
-						Manga
-					</NavLink>
-					<NavLink
-						className={({ isActive }) =>
-							isActive
-								? "media-page-link active-link-series"
-								: "media-page-link series"
-						}
-						to="/series"
-					>
-						Series
-					</NavLink>
-					<NavLink
-						className={({ isActive }) =>
-							isActive
-								? "media-page-link active-link-anime"
-								: "media-page-link anime"
-						}
-						to="/anime"
-					>
-						Anime
-					</NavLink>
-					<NavLink
-						className={({ isActive }) =>
-							isActive
-								? "media-page-link active-link-games"
-								: "media-page-link games"
-						}
-						to="/games"
-					>
-						Games
-					</NavLink>
-					<NavLink
-						className={({ isActive }) =>
-							isActive
-								? "media-page-link active-link-movies"
-								: "media-page-link movies"
-						}
-						to="/movies"
-					>
-						Movies
-					</NavLink>
-				</div>
+				<NavLink
+					className={({ isActive }) =>
+						isActive ? "active-media-page-link" : ""
+					}
+					to="/books"
+				>
+					Books
+				</NavLink>
+				<NavLink
+					className={({ isActive }) =>
+						isActive ? "active-media-page-link" : ""
+					}
+					to="/manga"
+				>
+					Manga
+				</NavLink>
+				<NavLink
+					className={({ isActive }) =>
+						isActive ? "active-media-page-link" : ""
+					}
+					to="/series"
+				>
+					Series
+				</NavLink>
+				<NavLink
+					className={({ isActive }) =>
+						isActive ? "active-media-page-link" : ""
+					}
+					to="/anime"
+				>
+					Anime
+				</NavLink>
+				<NavLink
+					className={({ isActive }) =>
+						isActive ? "active-media-page-link" : ""
+					}
+					to="/games"
+				>
+					Games
+				</NavLink>
+				<NavLink
+					className={({ isActive }) =>
+						isActive ? "active-media-page-link" : ""
+					}
+					to="/movies"
+				>
+					Movies
+				</NavLink>
 				<button className="quick-add-button">
 					<p>+</p>
 				</button>
