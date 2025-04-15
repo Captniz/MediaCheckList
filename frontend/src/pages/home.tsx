b2cccd2478fe1956127bbf9b90a9d69fb1d41052
Simone Tardiani <simone.tardiani.imp@gmail.com>
2025-04-15 19:53:36 +0200

    added home dummy elements for testing

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -6,78 +6,96 @@ const Home = () => {
 	return (
 		<div className="home">
 			<Navbar />
-			<ListElement
-				totUnits={100}
-				completedUnits={50}
-				status="On Hold"
-				genre={"Genre 1"}
-				saga={"Saga 1"}
-				notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
-			/>
-			<ListElement
-				totUnits={100}
-				completedUnits={50}
-				status="On Hold"
-				genre={"Genre 1"}
-				saga={"Saga 1"}
-				notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
-			/>
-			<ListElement
-				totUnits={100}
-				completedUnits={50}
-				status="On Hold"
-				genre={"Genre 1"}
-				saga={"Saga 1"}
-				notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
-			/>
-			<HomeMediaSect defaultCss="manga" itemsFound={100}>
+			<HomeMediaSect sectionName="books" itemsFound={100}>
 				<ListElement
 					totUnits={1000}
 					completedUnits={100}
 					status="Planned"
 					genre={"Genre 1"}
 					saga={"Saga 1"}
-					notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
+					notes={
+						"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"
+					}
 				/>
 			</HomeMediaSect>
-			<HomeMediaSect defaultCss="series" itemsFound={100}>
+			<HomeMediaSect sectionName="manga" itemsFound={100}>
+				<ListElement
+					totUnits={1000}
+					completedUnits={100}
+					status="Planned"
+					genre={"Genre 1"}
+					saga={"Saga 1"}
+					notes={
+						"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"
+					}
+				/>
+			</HomeMediaSect>
+			<HomeMediaSect sectionName="series" itemsFound={100}>
+			<ListElement
+					totUnits={1000}
+					completedUnits={100}
+					status="Active"
+					genre={"Genre 1"}
+					saga={"Saga 1"}
+					notes={
+						"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"
+					}
+				/>
 				<ListElement
 					totUnits={1000}
 					completedUnits={100}
 					status="Active"
 					genre={"Genre 1"}
 					saga={"Saga 1"}
-					notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
+					notes={
+						"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"
+					}
+				/>
+				<ListElement
+					totUnits={1000}
+					completedUnits={100}
+					status="Active"
+					genre={"Genre 1"}
+					saga={"Saga 1"}
+					notes={
+						"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"
+					}
 				/>
 			</HomeMediaSect>
-			<HomeMediaSect defaultCss="anime" itemsFound={100}>
+			<HomeMediaSect sectionName="anime" itemsFound={100}>
 				<ListElement
 					totUnits={1000}
 					completedUnits={100}
 					status="Completed"
 					genre={"Genre 1"}
 					saga={"Saga 1"}
-					notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
+					notes={
+						"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"
+					}
 				/>
 			</HomeMediaSect>
-			<HomeMediaSect defaultCss="games" itemsFound={100}>
+			<HomeMediaSect sectionName="games" itemsFound={100}>
 				<ListElement
 					totUnits={1000}
 					completedUnits={100}
 					status="Planned"
 					genre={"Genre 1"}
 					saga={"Saga 1"}
-					notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
+					notes={
+						"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"
+					}
 				/>
 			</HomeMediaSect>
-			<HomeMediaSect defaultCss="movies" itemsFound={100}>
+			<HomeMediaSect sectionName="movies" itemsFound={100}>
 				<ListElement
 					totUnits={1000}
 					completedUnits={100}
 					status="Planned"
 					genre={"Genre 1"}
 					saga={"Saga 1"}
-					notes={"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"}
+					notes={
+						"# Hello Markdown!\nThis is **bold** and this is *italic*.\n- This is a list\n- This is another list\n- This is a third list\n\n[This is a link](https://example.com)\n\n![This is an image](https://example.com/image.png)\n\n```javascript\nconst x = 1;\nconsole.log(x);\n```\n\n> This is a blockquote\n\n---\n\nThis is a horizontal rule.\n"
+					}
 				/>
 			</HomeMediaSect>
 		</div>
